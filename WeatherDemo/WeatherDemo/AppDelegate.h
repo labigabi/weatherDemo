66437c5c85f4071b484bb90df50df746ea60894b
Peter Stojcsics <peter@liligo.com>
2014-08-08 08:55:50 +0200

    project creation

---
+++ WeatherDemo/WeatherDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WeatherDemo
+//
+//  Created by Peter Stojcsics on 2014.08.08..
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WeatherDemo/WeatherDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherDemo
+//
+//  Created by Peter Stojcsics on 2014.08.08..
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
